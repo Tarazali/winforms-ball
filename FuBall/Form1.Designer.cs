5c56a631ac5e10e2cb7c89be2b7094f63fbc896a
Tarazali Ryskul <tarazaliryskulov@gmail.com>
2016-09-05 05:59:19 +0600

    Winforms ball

---
+++ FuBall/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FuBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.ball);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(666, 378);
+            this.playground.TabIndex = 0;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ball.Location = new System.Drawing.Point(274, 157);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 378);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FuBall/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuBall
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            
+            //this.TopMost = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+            if(ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if(ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if(ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if(ball.Bottom >= playground.Bottom)
+            {
+                speed_top = -speed_top;
+            }
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
